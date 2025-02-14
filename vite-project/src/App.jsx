223bec7926a7df5a7183be5c1cecdbd533dab0eb
Ashwin Annamalai Lena <ashwinannamalai2020@gmail.com>
2025-02-14 20:48:18 +0530

    day02_Commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' //External CSS
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { //function creation
+  let name = "Ashwin Annamalai"
+  
+  const infoDetails={  //object calling 
+    name: "Ashwin Annamalai Lena",
+    age: 20,
+    dept: "M.tech CSE"
+  }
+  //print -> alert or console.log
+  alert(`my name is ${infoDetails.name}`)
+  console.log(`My age is ${infoDetails.age}`)
 
-  return (
+  // return somthing
+  return ( 
+    // browser display
     <>
-      <h1>Hello World</h1>
+      <h1 style={{textAlign:"center"}}>My Department is {infoDetails.dept}</h1>  {/*inline style*/} 
+      
+      {/* <h1 style={rStyle}>Ashwin</h1> 
+       internal style  */}
+      {/* <h1>{infoDetails.name}</h1> */}
+      {/* <p>my name is {name}</p> */}
+      <p className='infoDetailsStyles'>Name: {infoDetails.name}<br/>  {/* external css */}
+      
+        Age: {infoDetails.age}<br/>
+        Department: {infoDetails.dept}<br/>
+      </p>
     </>
   )
 }
 
 export default App
+
